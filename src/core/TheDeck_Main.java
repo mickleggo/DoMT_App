149e1aef762007d57a7df50d503fef830d8d753e
mickleggo <mickleggo@hotmail.com>
2023-12-17 23:40:05 +1030

    Added in card removal, and logic to deal with empty deck

--- src/core/TheDeck_Main.java
+++ src/core/TheDeck_Main.java
@@ -1,10 +1,33 @@
 package core;
+import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
+
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
-import theDeck.*;
+
+import theDeck.Balance;
+import theDeck.Comet;
+import theDeck.Donjon;
+import theDeck.Euryale;
+import theDeck.Fates;
+import theDeck.Flames;
+import theDeck.Gem;
+import theDeck.Idiot;
+import theDeck.Jester;
+import theDeck.Rogue;
+import theDeck.Ruin;
+import theDeck.Skull;
+import theDeck.Talon;
+import theDeck.The_Fool;
+import theDeck.The_Key;
+import theDeck.The_Knight;
+import theDeck.The_Moon;
+import theDeck.The_Star;
+import theDeck.The_Sun;
+import theDeck.Throne;
+import theDeck.Vizier;
 import theDeck.Void;
 
 public class TheDeck_Main {
@@ -16,7 +39,8 @@ public class TheDeck_Main {
 	private MenuHandler mHandler = new MenuHandler();
 	private CustomMouseListener dHandler = new CustomMouseListener();
 	private BufferedImage image;
-	private String[] inDeck = new String[22];
+	private boolean[] inDeck = new boolean[22];
+	private boolean cardsRemain = true;
 
 	private int CardColumn = 5; // 0 to 5 - with 5 for Back of cards
 	private int CardRow = 0; // 0 to 3 - with 0 for Back of cards
@@ -32,7 +56,7 @@ public class TheDeck_Main {
 		spritectrl.ImportDeck();
 
 		for (int i = 0; i < inDeck.length; i++) {
-
+			inDeck[i] = true;
 		}
 
 		getImage();
@@ -45,11 +69,24 @@ public class TheDeck_Main {
 	}
 
 	public void drawCard() {
-		int ran = (int) (Math.random() * 22 + 1);
-
+		if(!cardsRemain) {
+			System.out.println("No cards remaining!");
+			this.CardColumn = 5;
+			this.CardRow = 3;
+			getImage();
+			cardDrawn.CardGUI(image, "Deck is Empty");
+			return;
+		}
+		
+		int ran = 0;
+		do {
+			ran = (int) (Math.random() * 22 + 1);
+		} while(inDeck[ran-1] == false);
+		
 		switch (ran) {
 		case 1: // The Key
 			new The_Key();
+			inDeck[ran-1] = false;
 			this.CardColumn = The_Key.getCardCol();
 			this.CardRow = The_Key.getCardRow();
 			this.cardName = The_Key.getCardName();
@@ -59,6 +96,7 @@ public class TheDeck_Main {
 
 		case 2: // Comet
 			new Comet();
+			inDeck[ran-1] = false;
 			this.CardColumn = Comet.getCardCol();
 			this.CardRow = Comet.getCardRow();
 			this.cardName = Comet.getCardName();
@@ -68,6 +106,7 @@ public class TheDeck_Main {
 
 		case 3: // Flames
 			new Flames();
+			inDeck[ran-1] = false;
 			this.CardColumn = Flames.getCardCol();
 			this.CardRow = Flames.getCardRow();
 			this.cardName = Flames.getCardName();
@@ -77,6 +116,7 @@ public class TheDeck_Main {
 
 		case 4: // Idiot
 			new Idiot();
+			inDeck[ran-1] = false;
 			this.CardColumn = Idiot.getCardCol();
 			this.CardRow = Idiot.getCardRow();
 			this.cardName = Idiot.getCardName();
@@ -86,6 +126,7 @@ public class TheDeck_Main {
 
 		case 5: // Fates
 			new Fates();
+			inDeck[ran-1] = false;
 			this.CardColumn = Fates.getCardCol();
 			this.CardRow = Fates.getCardRow();
 			this.cardName = Fates.getCardName();
@@ -95,6 +136,7 @@ public class TheDeck_Main {
 
 		case 6: // Donjon
 			new Donjon();
+			inDeck[ran-1] = false;
 			this.CardColumn = Donjon.getCardCol();
 			this.CardRow = Donjon.getCardRow();
 			this.cardName = Donjon.getCardName();
@@ -104,6 +146,7 @@ public class TheDeck_Main {
 
 		case 7: // Throne
 			new Throne();
+			inDeck[ran-1] = false;
 			this.CardColumn = Throne.getCardCol();
 			this.CardRow = Throne.getCardRow();
 			this.cardName = Throne.getCardName();
@@ -113,6 +156,7 @@ public class TheDeck_Main {
 
 		case 8: // The Moon
 			new The_Moon();
+			inDeck[ran-1] = false;
 			this.CardColumn = The_Moon.getCardCol();
 			this.CardRow = The_Moon.getCardRow();
 			this.cardName = The_Moon.getCardName();
@@ -122,6 +166,7 @@ public class TheDeck_Main {
 
 		case 9: // The Star
 			new The_Star();
+			inDeck[ran-1] = false;
 			this.CardColumn = The_Star.getCardCol();
 			this.CardRow = The_Star.getCardRow();
 			this.cardName = The_Star.getCardName();
@@ -131,6 +176,7 @@ public class TheDeck_Main {
 
 		case 10: // Skull
 			new Skull();
+			inDeck[ran-1] = false;
 			this.CardColumn = Skull.getCardCol();
 			this.CardRow = Skull.getCardRow();
 			this.cardName = Skull.getCardName();
@@ -140,6 +186,7 @@ public class TheDeck_Main {
 
 		case 11: // Rogue
 			new Rogue();
+			inDeck[ran-1] = false;
 			this.CardColumn = Rogue.getCardCol();
 			this.CardRow = Rogue.getCardRow();
 			this.cardName = Rogue.getCardName();
@@ -149,6 +196,7 @@ public class TheDeck_Main {
 
 		case 12: // Euryale
 			new Euryale();
+			inDeck[ran-1] = false;
 			this.CardColumn = Euryale.getCardCol();
 			this.CardRow = Euryale.getCardRow();
 			this.cardName = Euryale.getCardName();
@@ -158,6 +206,7 @@ public class TheDeck_Main {
 
 		case 13: // Vizier
 			new Vizier();
+			inDeck[ran-1] = false;
 			this.CardColumn = Vizier.getCardCol();
 			this.CardRow = Vizier.getCardRow();
 			this.cardName = Vizier.getCardName();
@@ -167,6 +216,7 @@ public class TheDeck_Main {
 
 		case 14: // The Knight
 			new The_Knight();
+			inDeck[ran-1] = false;
 			this.CardColumn = The_Knight.getCardCol();
 			this.CardRow = The_Knight.getCardRow();
 			this.cardName = The_Knight.getCardName();
@@ -176,6 +226,7 @@ public class TheDeck_Main {
 
 		case 15: // Jester
 			new Jester();
+			inDeck[ran-1] = false;
 			this.CardColumn = Jester.getCardCol();
 			this.CardRow = Jester.getCardRow();
 			this.cardName = Jester.getCardName();
@@ -185,6 +236,7 @@ public class TheDeck_Main {
 
 		case 16: // The Sun
 			new The_Sun();
+			inDeck[ran-1] = false;
 			this.CardColumn = The_Sun.getCardCol();
 			this.CardRow = The_Sun.getCardRow();
 			this.cardName = The_Sun.getCardName();
@@ -194,6 +246,7 @@ public class TheDeck_Main {
 
 		case 17: // The Fool
 			new The_Fool();
+			inDeck[ran-1] = false;
 			this.CardColumn = The_Fool.getCardCol();
 			this.CardRow = The_Fool.getCardRow();
 			this.cardName = The_Fool.getCardName();
@@ -203,6 +256,7 @@ public class TheDeck_Main {
 
 		case 18: // Balance
 			new Balance();
+			inDeck[ran-1] = false;
 			this.CardColumn = Balance.getCardCol();
 			this.CardRow = Balance.getCardRow();
 			this.cardName = Balance.getCardName();
@@ -212,6 +266,7 @@ public class TheDeck_Main {
 
 		case 19: // Gem
 			new Gem();
+			inDeck[ran-1] = false;
 			this.CardColumn = Gem.getCardCol();
 			this.CardRow = Gem.getCardRow();
 			this.cardName = Gem.getCardName();
@@ -221,6 +276,7 @@ public class TheDeck_Main {
 
 		case 20: // Ruin
 			new Ruin();
+			inDeck[ran-1] = false;
 			this.CardColumn = Ruin.getCardCol();
 			this.CardRow = Ruin.getCardRow();
 			this.cardName = Ruin.getCardName();
@@ -230,6 +286,7 @@ public class TheDeck_Main {
 
 		case 21: // Talon
 			new Talon();
+			inDeck[ran-1] = false;
 			this.CardColumn = Talon.getCardCol();
 			this.CardRow = Talon.getCardRow();
 			this.cardName = Talon.getCardName();
@@ -239,6 +296,7 @@ public class TheDeck_Main {
 
 		case 22: // Void
 			new Void();
+			inDeck[ran-1] = false;
 			this.CardColumn = Void.getCardCol();
 			this.CardRow = Void.getCardRow();
 			this.cardName = Void.getCardName();
@@ -246,6 +304,12 @@ public class TheDeck_Main {
 			cardDrawn.CardGUI(image, this.cardName);
 			break;
 		}
+		
+		cardsRemain = false;
+		for(int i = 0; i < inDeck.length ; i++) {
+			if(inDeck[i]) cardsRemain = true;
+		}
+
 
 	}
 
